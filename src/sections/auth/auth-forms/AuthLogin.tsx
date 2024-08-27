e2e8d55d4c3229e8b3ac09e4515116cce5497e16
mattral <minhtetmyet2630@gmail.com>
2024-08-27 20:16:46 +0700

    disabled={isSubmitting} rmv

--- src/sections/auth/auth-forms/AuthLogin.tsx
+++ src/sections/auth/auth-forms/AuthLogin.tsx
@@ -241,7 +241,7 @@ const AuthLogin = ({ forgot }: AuthLoginProps) => {
             )}
             <Grid item xs={12}>
               <AnimateButton>
-                <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
+                <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="primary">
                   Login
                 </Button>
               </AnimateButton>
