8e04199b80ea9aad7ac75fcbadfb407686f6050c
mattral <minhtetmyet2630@gmail.com>
2024-09-16 11:22:12 +0700

    css fix

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,39 +9,3 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 }
 
 
-
-/*import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import GuestGuard from 'utils/route-guard/GuestGuard';
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <GuestGuard>
-          {children}
-          </GuestGuard>
-        </body>
-      </html>
-    </ClerkProvider>
-  )
-}
-*/
-
-
