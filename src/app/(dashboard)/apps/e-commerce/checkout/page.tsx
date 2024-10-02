33f418e5694b93ee807f5b4ddf6d924a2b8f4812
David Gonm <148528760+DavidMacha@users.noreply.github.com>
2024-10-02 15:37:39 +0700

    Update page.tsx

--- src/app/(dashboard)/apps/e-commerce/checkout/page.tsx
+++ src/app/(dashboard)/apps/e-commerce/checkout/page.tsx
@@ -2,7 +2,7 @@
 
 // PROJECT IMPORTS
 //import AddNewProduct from 'views/apps/AddNewProduct';
-//import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 
 const PreviewContainer = dynamic(() => import("feature/preview/preview"), {
   ssr: false,
