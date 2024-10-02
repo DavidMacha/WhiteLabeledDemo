0b7fe8ba2583758783441e82887d7daac5e1590e
David Gonm <148528760+DavidMacha@users.noreply.github.com>
2024-10-02 15:16:43 +0700

    Update page.tsx\

--- src/app/(dashboard)/apps/e-commerce/checkout/page.tsx
+++ src/app/(dashboard)/apps/e-commerce/checkout/page.tsx
@@ -1,10 +1,11 @@
 // PROJECT IMPORTS
-import AddNewProduct from 'views/apps/AddNewProduct';
+//import AddNewProduct from 'views/apps/AddNewProduct';
+import PreviewContainer from 'feature/preview/preview.tsx'
 
 // ==============================|| ECOMMERCE - ADD PRODUCT ||============================== //
 
 function AddNewProductPage() {
-  return <AddNewProduct />;
+  return <PreviewContainer />;
 }
 
 export default AddNewProductPage;
