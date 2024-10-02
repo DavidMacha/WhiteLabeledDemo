d900a277012e516b6bc4121dc937d7d048508e97
David Gonm <148528760+DavidMacha@users.noreply.github.com>
2024-10-02 15:30:44 +0700

    Update page.tsx\

--- src/app/(dashboard)/apps/e-commerce/checkout/page.tsx
+++ src/app/(dashboard)/apps/e-commerce/checkout/page.tsx
@@ -2,6 +2,11 @@
 
 // PROJECT IMPORTS
 //import AddNewProduct from 'views/apps/AddNewProduct';
+import dynamic from "next/dynamic";
+
+const DynamicBundledEditor = dynamic(() => import("../../components/BundledEditor"), {
+  ssr: false,
+});
 import PreviewContainer from 'feature/preview/preview'
 
 // ==============================|| ECOMMERCE - ADD PRODUCT ||============================== //
